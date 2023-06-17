f7ce0c2d2b93fe3a797254ae6a59745d73642590
Andrey <trancemilly7@gmail.com>
2023-06-17 11:00:36 +0200

    adds mobile styles

--- src/App.jsx
+++ src/App.jsx
@@ -29,22 +29,22 @@ function App() {
         your audience. Create connections with your users as you engage in genuine discussion. 
       </p>
       <button className='header-btn'>Get Started For Free</button>
-      <img className='header-img' src='/images/screen-mockups.svg'></img>
+      <img className='header-img' src='/images/screen-mockups.svg' alt='Illustration'></img>
     </section>
     <section className='stats-section'>
       <div className='stats-item'>
-        <img className='stats-img' src='/images/icon-communities.svg'/>
+        <img className='stats-img' src='/images/icon-communities.svg' alt='Icon'/>
         <h1 className='stats-number'>1.4k+</h1>
         <p className='stats-text'>Communities Formed</p>
       </div>
       <div className='stats-item'>
-        <img className='stats-img' src='/images/icon-messages.svg'/>
+        <img className='stats-img' src='/images/icon-messages.svg' alt='Icon'/>
         <h1 className='stats-number'>2.7m+</h1>
         <p className='stats-text'>Messages Sent</p>
       </div>
     </section>
     <section className='grow-section'>
-      <img className='grow-img' src='/images/illustration-grow-together.svg'></img>
+      <img className='grow-img' src='/images/illustration-grow-together.svg' alt='Illustration'></img>
       <div className='grow-content'>
         <h1 className='grow-title'>Grow Together</h1>
         <p className='grow-text'>
@@ -54,21 +54,21 @@ function App() {
       </div>
     </section>
     <section className='flowing-section'>
-      <img className='flowing-img' src='/images/illustration-flowing-conversation.svg'></img>
+      <img className='flowing-img' src='/images/illustration-flowing-conversation.svg' alt='Illustration'></img>
       <div className='flowing-content'>
         <h1 className='flowing-title'>Flowing Conversations</h1>
         <p className='flowing-text'>
-          You wouldn't paginate a conversation in real life, so why do it online? Our threads have 
+          You wouldn&#39;t paginate a conversation in real life, so why do it online? Our threads have 
           just-in-time loading for a more natural flow.
         </p>
       </div>
     </section>
     <section className='users-section'>
-      <img className='users-img' src='/images/illustration-your-users.svg'></img>
+      <img className='users-img' src='/images/illustration-your-users.svg' alt='Illustration'></img>
       <div className='users-content'>
         <h1 className='users-title'>Your Users</h1>
         <p className='users-text'>
-          It takes no time at all to integrate Huddle with your app's authentication solution. This means, 
+          It takes no time at all to integrate Huddle with your app&#39;s authentication solution. This means, 
           once signed in to your app, your users can start chatting immediately.
         </p>
       </div>
@@ -79,17 +79,17 @@ function App() {
     </section>
     <section className='footer-section'>
       <div className='footer-contacts'>
-        <img className='footer-logo' src='/images/logo.svg'></img>
+        <img className='footer-logo' src='/images/logo.svg' alt='Logo'></img>
         <p className='footer-text'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia 
           vestibulum a, ultrices quis sem.
         </p>
         <div className='contact-item'>
-          <img className='contact-icon' src='/images/icon-phone.svg'></img>
+          <img className='contact-icon' src='/images/icon-phone.svg' alt='Icon'></img>
           <p className='contact-text'>Phone: +1-543-123-4567</p>
         </div>
         <div className='contact-item'>
-          <img className='contact-icon' src='/images/icon-email.svg'></img>
+          <img className='contact-icon' src='/images/icon-email.svg' alt='Icon'></img>
           <p className='contact-text'>example@huddle.com</p>
         </div>
         <div className='socials'>
@@ -101,7 +101,7 @@ function App() {
       <div className='subscribe'>
         <h1 className='subscribe-title'>Newsletter</h1>
         <p className='subscribe-text'>
-          To recieve tips on how to grow your community, sign up to our weekly newsletter. We’ll never 
+          To recieve tips on how to grow your community, sign up to our weekly newsletter. We&#39;ll never 
           send you spam or pass on your email address
         </p>
         <div className='subscribe-input-container'>
@@ -114,7 +114,7 @@ function App() {
       </div>
       <p className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/aveandrian/">aveandrian</a>.
       </p>
     </section>
     </>
